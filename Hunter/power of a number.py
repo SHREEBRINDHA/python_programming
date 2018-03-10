148ad11cb232d2cb86ccd27b2764b9537804d7a8
SHREEBRINDHA <36122231+SHREEBRINDHA@users.noreply.github.com>
2018-03-10 17:56:20 +0530

    Update power of a number.py

--- Hunter/power of a number.py
+++ Hunter/power of a number.py
@@ -1,8 +1,8 @@
-def pow(base,exp):
+def pow1(base,exp):
     if(exp==1):
         return(base)
     if(exp!=1):
-        return(base*pow(base,exp-1))
+        return(base*pow1(base,exp-1))
 base=int(input("Enter base: "))
 exp=int(input("Enter exponential value: "))
-print("Result:",pow(base,exp))
+print("Result:",pow1(base,exp))
