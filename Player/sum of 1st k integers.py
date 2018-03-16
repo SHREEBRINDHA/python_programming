963dd74f26338c34537747d4792c892e629ff8fa
SHREEBRINDHA <36122231+SHREEBRINDHA@users.noreply.github.com>
2018-03-16 18:14:37 +0530

    Update sum of 1st k integers.py

--- Player/sum of 1st k integers.py
+++ Player/sum of 1st k integers.py
@@ -1,9 +1,9 @@
 x=[]
 n=int(input(""))
 k=int(input(""))
-sum=0
+sum1=0
 for i in range(n):
     x.append(int(input()))
 for i in range(k):
-    sum=sum+x[i]
-print(sum)
+    sum1=sum1+x[i]
+print(sum1)
